ca6f92a6760b06d3ab01c1affd7db8d7333dae05
Rostyslav Skrypnyk <rostyslav.skrypnyk@gmail.com>
2019-03-10 20:11:30 +0100

    Debugged code to work for task B when cross-sectional areas of trusses are different. Completed report on task B. Results of task C are wrong right now.

--- nonlinear-truss/material_routine.m
+++ nonlinear-truss/material_routine.m
@@ -1,5 +1,5 @@
-function [stress, mat_stiff, params] = ...
-    material_routine(params, strain, analysis_type, el)
+function [stress, mat_stiff, state] = ...
+    material_routine(params, state, strain, analysis_type)
 %% MATERIAL_ROUTINE Returns stress and material stiffness.
 % Inputs:
 % params - structure containing material parameters.
@@ -12,11 +12,11 @@ function [stress, mat_stiff, params] = ...
 % mat_stiffness - derivative of stress w.r.t. strain.
 % params - updated input structure (Matlab sends a copy).
 
-stress_trial = params.young * (strain - params.plast_strain(el));
+stress_trial = params.young * (strain - state.plast_strain);
 trial_yield_f = eval_yield_func(stress_trial, ...
                                 params.yield, ...
                                 params.plast_mod, ...
-                                params.harden_param(el));
+                                state.harden_param);
 
 if strcmp(analysis_type, 'elastic') || trial_yield_f <= 0
     stress = stress_trial;
@@ -27,8 +27,8 @@ elseif strcmp(analysis_type, 'plastic') % trial_yield_f > 0 is implied.
     incr_pl_mult = trial_yield_f / (params.young + params.plast_mod);
     Dstrain_pl = incr_pl_mult * stress_trial / ( abs(stress_trial) + 1.e-10 );
     stress = stress_trial - params.young * Dstrain_pl;
-    params.plast_strain(el) = params.plast_strain(el) + Dstrain_pl;
-    params.harden_param(el) = params.harden_param(el) + incr_pl_mult;
+    state.plast_strain = state.plast_strain + Dstrain_pl;
+    state.harden_param = state.harden_param + incr_pl_mult;
     % Algorithmic tangent modulus:
     mat_stiff = (params.young * params.plast_mod) / (params.young + params.plast_mod);
 else
--- nonlinear-truss/nonlinear_truss.m
+++ nonlinear-truss/nonlinear_truss.m
@@ -11,9 +11,19 @@ clc
 addpath(genpath('~/Documents/MATLAB/calfem-3/')) % Add Calfem routines.
 
 %% Pre-processing
+task = 'C'; % Task # (A, B or C) for setting element area and saving files.
+angle_factor = 0.25;
+if strcmp(task,'A')
+    area_factor = 1;
+else
+    area_factor = 1.05;
+    if strcmp(task,'C')
+        angle_factor = 0.1025; % tan(alpha) = 1/3;
+    end
+end
 % Choose between hyperelastic material model and 
 % elasto-plastic model:
-analysis_type = 'plastic'; % 'elastic' or 'plastic'
+analysis_type = 'elastic'; % 'elastic' or 'plastic'
 if strcmp(analysis_type, 'elastic')
     fprintf('ELASTIC analysis was chosen.\n')
 elseif strcmp(analysis_type, 'plastic')
@@ -25,9 +35,9 @@ end
 save_to_file = false; % Save plot data.
 
 % Geometry of the structure:
-alpha = 0.25 * pi; % Truss angle from the task description.
+alpha = angle_factor * pi; % Truss angle from the task description.
 coord = [0     0;
-         100/tan(alpha) 100/tan(alpha);
+         100 100/tan(alpha);
          200   0]; % CALFEM: Global coordinate matrix, [mm].
 dof = [1 2; % dofs of node 1.
        3 4;
@@ -44,9 +54,9 @@ N_el_dof = 2; % Number of DOFs per node.
 % Load and time stepping:
 u_y = -2*norm(coord(1,:)-coord(2,:)); % Displace by twice the initial
                                       % length (see fig. 3.8b on p.90).
-dt = 0.01; % [s].
+dt = 0.0005; % [s]. Many steps are needed when truss areas are different.
 total_t = 1; % [s].
-N_steps = total_t / dt;
+N_steps = round(total_t / dt);
 
 % Velocity boundary conditions: velocity is used to conveniently set
 % prescribed value by multiplying with time step.
@@ -60,13 +70,17 @@ dof_free = setdiff( 1:N_dof, bc(:,1), 'stable'  ); % Free DOFs.
 
 % Material and cross-sectional parameters:
 params.areas = [0.5; ...% Cross-sectional areas of the bar elements, [mm^2].
-                0.5];
+                0.5 * area_factor];
 params.young = 210.e3; % Young's modulus, [N/mm^2] (see p.89).
 params.nu = 0.5; % Poisson's ratio.
 params.yield = 25.e3; % Yield stress, [N/mm^2].
 params.plast_mod = 1; % Plastic modulus H, [N/mm^2] (smth small compared to Young's modulus).
-params.plast_strain = zeros(N_el,1); % Plastic strain for each element.
-params.harden_param = zeros(N_el,1); % Hardening parameter (accumulated absolute plastic strain) for each element.
+
+state_array = struct('plast_strain',repmat({0},1,N_el), ...
+                     'harden_param',repmat({0},1,N_el));
+state_array_new = state_array;
+%state.plast_strain = zeros(N_el,1); % Plastic strain for each element.
+%state.harden_param = zeros(N_el,1); % Hardening parameter (accumulated absolute plastic strain) for each element.
 
 % Tolerances:
 error_tol = 1.e-6; % Error tolerance for Newton's iteration.
@@ -104,10 +118,10 @@ for step=1:N_steps % Time stepping.
         for i=1:N_el % Loop over elements:
             [force,K,...
              stress, strain,...
-             params] = element_routine(el_x(i,:), el_y(i,:), ...
-                                       u_el(i,:), du_el(i,:), ...
-                                       params, i, ...
-                                       analysis_type);           
+             state_array_new(i)] = element_routine(el_x(i,:), el_y(i,:), ...
+                                           u_el(i,:), du_el(i,:), ...
+                                           params, state_array(i), i, ...
+                                           analysis_type);           
             % Assemble global stiffness matrix and RHS vector:
             for ii = 1:N_el_nodes * N_el_dof 
                 for jj = 1:N_el_nodes * N_el_dof
@@ -117,19 +131,19 @@ for step=1:N_steps % Time stepping.
                     V(ind) = K(ii,jj);
                 end
             end
-           
+            
             residual_vector(e_dof(i,2:end)) = ...
                residual_vector(e_dof(i,2:end)) + force;
             % Save history:
             stress_hist(i,step) = stress;
             strain_hist(i,step) = strain;
-            strain_plast_hist(i,step) = params.plast_strain(i);
+            strain_plast_hist(i,step) = state_array_new(i).plast_strain;
         end
         K_system = sparse(I,J,V);
-        
+
         %% Solve the system of linear equations:
          delta_du = - K_system(dof_free,dof_free) \ ...
-                    residual_vector(dof_free); 
+                    residual_vector(dof_free);
         % Update displacement increment:
         du(dof_free) = du(dof_free) + delta_du;
         % Check if (internal - external) forces are zero:
@@ -139,9 +153,10 @@ for step=1:N_steps % Time stepping.
             break
         end
     end % Newton's iteration.
-    
+
     %% Update variables:
     u = u + du;
+    state_array = state_array_new;
     % Save history:
     u_hist(:,step) = u(dof_free);
     vertical_force_hist = [vertical_force_hist, vertical_force_hist(end) ...
@@ -158,15 +173,15 @@ figure(1) % Fig.3.8(b): vertical force vs deflection
 initial_length = norm([ el_x(1,2) - el_x(1,1); ... 
                         el_y(1,2) - el_y(1,1) ]);
 data_force = [ [0:dt*abs(u_y):abs(u_y)]' / initial_length,...
-                vertical_force_hist' / params.young / params.areas(1)/2 ]; % account for computing for 2 bars instead of 1.
+                vertical_force_hist' / params.young / sum(params.areas) ]; % account for computing for 2 bars instead of 1.
 plot(data_force(:,1), data_force(:,2),'o-')  
 xlabel('(Y - y) / L, [-]')
 ylabel('F / (EA) , [-]')
-xlim([0 2]) % Same limits as in Fig.3.8(b) of Bonet & Wood.
-ylim([-0.15 0.2])
+%xlim([0 2]) % Same limits as in Fig.3.8(b) of Bonet & Wood.
+%ylim([-0.15 0.2])
 grid on
 if save_to_file % Create file for LaTeX.
-    f_name = ['../doc/data/force_deflection_', analysis_type, '.dat'];
+    f_name = ['../doc/data/force_deflection_', analysis_type, '_', task, '.dat'];
     f_id = fopen(f_name,'w');
     header = '# (Y - y) / L, [-]   F / (EA), [-]';
     
@@ -187,7 +202,7 @@ xlim([-0.4 0.3]) % Same limits as in Fig.3.8(c).
 ylim([-30 30])
 grid on
 if save_to_file % Create file for LaTeX.
-    f_name = ['../doc/data/stress_strain_', analysis_type, '.dat'];
+    f_name = ['../doc/data/stress_strain_', analysis_type, '_', task, '.dat'];
     f_id = fopen(f_name,'w');
     header = '# strain, [-]   Kirchhoff stress, [kN/mm2]';
     
@@ -206,15 +221,37 @@ if strcmp(analysis_type, 'plastic')
     xlabel('Total strain, [-]')
     ylabel('Plastic strain, [-]')
     xlim([-0.4 0.3]) % Same limits as in Fig.3.8(d).
-    ylim([-0.25 0.15])
+    ylim([-0.27 0.15])
     grid on
     if save_to_file % Create file for LaTeX.
-        f_name = '../doc/data/tot_pl_strain.dat';
+        f_name = ['../doc/data/tot_pl_strain_', task,'.dat'];
         f_id = fopen(f_name,'w');
         header = '# total strain, [-]   plastic strain, [-]';
     
         fprintf(f_id, '%s\n', header);
         fprintf(f_id, '%.4f           %.4f\n', data_strain');
+        fclose(f_id);
+    end
+end
+
+
+
+if ~strcmp(task,'A')
+    figure() % Upper joing movement
+%     plot(coord(1:2,1), coord(1:2,2),'bo--') % Plot left truss.
+%     hold on
+%     data_motion = [ [coord(2,1) + [ zeros(length(dof_free),1), u_hist(1,:) ],coord(1,1)]', ...
+%                     [coord(2,2) + [0:dt*u_y:u_y], coord(1,2)]' ];
+    data_motion = [ [ zeros(length(dof_free),1), u_hist(1,:) ]', ...
+                    [0:dt*u_y:u_y]' ];
+    plot(data_motion(:,1),data_motion(:,2),'bo-')
+    if save_to_file % Create file for LaTeX.
+        f_name = ['../doc/data/node_displ_', analysis_type, '_', task, '.dat'];
+        f_id = fopen(f_name,'w');
+        header = '# horiz displ, [mm]   vert displ, [mm]';
+    
+        fprintf(f_id, '%s\n', header);
+        fprintf(f_id, '%.4f           %.4f\n', data_motion');
         fclose(f_id);
     end
 end
